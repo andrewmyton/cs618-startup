8be1951453ecaf6444f15c4d44ed8bdc9a732ae9
andrewmyton <159046575+andrewmyton@users.noreply.github.com>
2025-09-16 13:25:53 +0000

    chore: start ch4

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,33 @@ const queryClient = new QueryClient()
 
 export function App() {
   return (
+<<<<<<< HEAD
     <QueryClientProvider client={queryClient}>
       <Blog />
     </QueryClientProvider>
+=======
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React + Node.js</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+>>>>>>> 3447941e (chore: start ch4)
   )
 }
